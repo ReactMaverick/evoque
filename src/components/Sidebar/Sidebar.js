b9fd276879b49f88eabe21bb6ca2d750fdb9e584
Barun Karmakar <kumarbarun137@gmail.com>
2025-06-20 18:55:23 +0530

    20.06.2025

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,15 @@
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <h2>Sidebar</h2>
+            <ul>
+                <li><a href="#section1">Section 1</a></li>
+                <li><a href="#section2">Section 2</a></li>
+                <li><a href="#section3">Section 3</a></li>
+                <li><a href="#section4">Section 4</a></li>
+                <li><a href="#section5">Section 5</a></li>
+                <li><a href="#section6">Section 6</a></li>
+            </ul>
+        </div>
+    );
+}
