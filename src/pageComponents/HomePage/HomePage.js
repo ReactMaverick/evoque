ef983d0ec1363d5207703971da96b6a0c29805cf
Barun Karmakar <kumarbarun137@gmail.com>
2025-06-20 23:41:40 +0530

    20.06.2025

--- src/pageComponents/HomePage/HomePage.js
+++ src/pageComponents/HomePage/HomePage.js
@@ -1,9 +1,11 @@
 import Sidebar from "@/components/Sidebar/Sidebar";
+import Dashboard from "./Dashboard";
 
 export default function HomePage() {
     return (
         <div className="home-page p-[2.5rem] bg-primary h-screen ">
             <Sidebar />
+            <Dashboard />
         </div>
     );
 }
